c5e8364a33d46051fc0c6851ec5b06c82b886d5b
Ricardo Ribeiro Lopes <ricardo.sao@hotmail.com>
2020-03-07 12:51:15 -0300

    Sistema de carrinho inicializado e mudança de layout

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import Data from '../data/db.json';
 import '../pages/home.css';
 
-let indisponivel = "";
+let carrinho= [];
 class Ecommerce extends Component {
     constructor() {
         super();
         this.state = {
             itens: [],
-            search: ''
+            search: '',    
+            carState: []        
         }
     }
+    //Funcão que verifica se minha
     retImageOrNotFound(item) {
         if (item != null && item != "") item = <img src={item} alt="imagem do produto"></img>
         return item;
@@ -18,6 +20,20 @@ class Ecommerce extends Component {
     //Atualizando o meu estado a cada letra digitada
     updateSearch(event) {
         this.setState({ search: event.target.value.substr(0, 20) });
+    }
+    retPrecoDisponivel(onSale, preco, precoPromocao) {
+        if (onSale == true) {
+            return (
+                <div>
+                    <strike style={{ fontSize: '0.8em' }}>De {preco}</strike>
+                    <p className="preco">Por {precoPromocao} </p>
+                </div>
+            )
+        } else return <p className="preco">Por {preco}</p>
+    }
+    addToCar(index) {
+        carrinho.push(index);
+        this.setState({ carState : carrinho })
     }
     render() {
         //criando o filtro
@@ -30,37 +46,45 @@ class Ecommerce extends Component {
             <div>
                 <h2>O melhor E-commerce para você</h2>
                 <input type="text" id="txtBusca" value={this.state.search} onChange={this.updateSearch.bind(this)} placeholder="Pesquisar produtos..." />
+                <p>Carrinho ({this.state.carState.length > 0 ? this.state.carState.length : '0' })</p>
                 <div className="container">
                     {/* Mapeando o meu array de products */}
-                    {DataFiltra.map((item, index) => {
-                        return (
-                            <div className="container-produtos" >
-                                <div className="image">
-                                    {this.retImageOrNotFound(item.image)}
-                                </div>
-                                <div className="container-informacoes">
-                                    <div className="informacoes">
-                                        <h3>{item.name}</h3>
-                                        {/* <p>{item.style}</p>
-                                        <p>{item.code_color}</p>
-                                        <p>{item.color_slug}</p> */}
-                                        <p>Cor {item.color}</p>
-                                        <p>{item.on_sale}</p>
-                                        <p>Preço regular {item.regular_price}</p>
-                                        <p>Preço atual {item.actual_price}</p>
-                                        {item.discount_percentage != "" ? <p>Desconto de {item.discount_percentage}</p> : ""}
-                                        <p>ou {item.installments}</p>
-                                        <p>Tamanho
-                                         <select> {item.sizes.map((size, index) => {
-                                            return (
-                                                <option disabled={size.available == true ? false : true} style={{ fontSize: '1em', marginLeft: '2px', color: (size.available == true ? '#4346FF' : '#839693') }} className="size">{size.size}</option>
-                                            )
-                                        })}</select></p>
+                    {DataFiltra.map((item, index, data) => {
+                        if (data.length == 0) {
+                            return <h3>Nenhum produto encontrado...</h3>
+                        }
+                        else {
+                            return (
+                                <div onClick={(event) => this.addToCar(event, index)} className="container-produtos" tabIndex="0">
+                                    <div className="image">
+                                        {this.retImageOrNotFound(item.image)}
                                     </div>
-                                    <div className="btn"><button>Adicionar ao Carrinho</button></div>
+                                    <div className="container-informacoes">
+                                        <div className="informacoes">
+                                            <h3>{item.name}</h3>
+                                            <p className="cor">{item.color}</p>
+                                            <div className="descricao">
+                                                {this.retPrecoDisponivel(item.on_sale, item.regular_price, item.actual_price, item.discount_percentage)}
+                                                <p>ou {item.installments}</p>
+                                                <p style={{ height: '1px' }}>Tamanhos</p>
+                                                <p style={{ display: 'flex', flexDirection: 'rows' }}>
+                                                    {item.sizes.map((size, index) => {
+                                                        return (
+                                                            <p style={{ fontSize: '1em', marginLeft: '2px', color: (size.available == true ? '#5A5FE8' : '#839693') }} className="size">{size.size}</p>
+                                                        )
+                                                    })}
+                                                    {/* <select> {item.sizes.map((size, index) => {                                             
+                                                return (
+                                                    <option disabled={size.available == true ? false : true} style={{ fontSize: '1em', marginLeft: '2px', color: (size.available == true ? '#4346FF' : '#839693') }} className="size">{size.size}</option>
+                                                )
+                                            })}</select> */}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        )
+                            )
+                        }
                     })}
                 </div>
             </div>
