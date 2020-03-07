27fe80ea49d245e4fd12a71adc1d37cd4922897e
Ricardo Ribeiro Lopes <ricardo.sao@hotmail.com>
2020-03-07 16:36:42 -0300

    Adicionando no carrinho e modal feito

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import Data from '../data/db.json';
 import '../pages/home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, Button, ButtonToolbar } from 'react-bootstrap'
 
-let carrinho= [];
+let carrinho = [];
+let itemSelected = [];
+let produtos = [];
 class Ecommerce extends Component {
     constructor() {
         super();
         this.state = {
             itens: [],
-            search: '',    
-            carState: []        
+            search: '',
+            carState: [],
+            modalShow: false,
+            prodSelected: ''
         }
     }
     //Funcão que verifica se minha
@@ -31,12 +37,59 @@ class Ecommerce extends Component {
             )
         } else return <p className="preco">Por {preco}</p>
     }
-    addToCar(index) {
-        carrinho.push(index);
-        this.setState({ carState : carrinho })
+    //Função que adiciona os itens escolhidos ao carrinho.
+    addToCar() {
+        carrinho.push(this.state.prodSelected);
+        this.setState({ carState: carrinho })
+        console.log(carrinho)
     }
+
+    chamaModal(item) {
+        this.setState({ modalShow: !this.state.modalShow });
+        this.setState({ prodSelected: null });
+        this.setState({ prodSelected: item });
+        itemSelected.push(item);
+        console.log(itemSelected);
+    }
+
+
     render() {
-        //criando o filtro
+        //Função do meu Modal
+        function MyVerticallyCenteredModal(props) {
+            return (
+                <Modal
+                    {...props}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            <p>{props.prod.name}</p>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <img style={{ width: '20%' }} src={props.prod.image}></img>
+                        <p>{props.prod.color}</p>
+                        <p>{props.prod.actual_price}</p>
+                        <p>{props.prod.size}</p>
+                        <p>{props.car.length}</p>
+                        {/* <select> {this.props.prod.sizes.map((size) => {
+                            return (
+                                <option disabled={size.available == true ? false : true} style={{ fontSize: '1em', marginLeft: '2px', color: (size.available == true ? '#4346FF' : '#839693') }} className="size">{size.size}</option>
+                            )
+                        })}</select> */}
+                        {console.log(props.prod.name)}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={props.onHide}>Fechar</Button>
+                        <Button onClick={props.add}>Adicionar</Button>
+                    </Modal.Footer>
+                </Modal>
+            );
+        }
+
+        //Criando o filtro
         var DataFiltra = Data.products.filter(
             (result) => {
                 return result.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
@@ -44,21 +97,29 @@ class Ecommerce extends Component {
         );
         return (
             <div>
+
                 <h2>O melhor E-commerce para você</h2>
                 <input type="text" id="txtBusca" value={this.state.search} onChange={this.updateSearch.bind(this)} placeholder="Pesquisar produtos..." />
-                <p>Carrinho ({this.state.carState.length > 0 ? this.state.carState.length : '0' })</p>
+                <p>Carrinho ({this.state.carState.length > 0 ? this.state.carState.length : '0'})</p>
                 <div className="container">
                     {/* Mapeando o meu array de products */}
                     {DataFiltra.map((item, index, data) => {
+                        produtos.push(item)
                         if (data.length == 0) {
                             return <h3>Nenhum produto encontrado...</h3>
                         }
                         else {
                             return (
-                                <div onClick={(event) => this.addToCar(event, index)} className="container-produtos" tabIndex="0">
+                                <div onClick={() => { this.chamaModal(item) }} className="container-produtos" tabIndex="0">
                                     <div className="image">
                                         {this.retImageOrNotFound(item.image)}
                                     </div>
+                                    <MyVerticallyCenteredModal
+                                        show={this.state.modalShow}
+                                        prod={this.state.prodSelected}
+                                        car={this.state.carState}
+                                        add={(event) => this.addToCar(event, this.state.prodSelected)}
+                                    />
                                     <div className="container-informacoes">
                                         <div className="informacoes">
                                             <h3>{item.name}</h3>
@@ -73,11 +134,6 @@ class Ecommerce extends Component {
                                                             <p style={{ fontSize: '1em', marginLeft: '2px', color: (size.available == true ? '#5A5FE8' : '#839693') }} className="size">{size.size}</p>
                                                         )
                                                     })}
-                                                    {/* <select> {item.sizes.map((size, index) => {                                             
-                                                return (
-                                                    <option disabled={size.available == true ? false : true} style={{ fontSize: '1em', marginLeft: '2px', color: (size.available == true ? '#4346FF' : '#839693') }} className="size">{size.size}</option>
-                                                )
-                                            })}</select> */}
                                                 </p>
                                             </div>
                                         </div>
@@ -85,7 +141,8 @@ class Ecommerce extends Component {
                                 </div>
                             )
                         }
-                    })}
+                    }
+                    )}
                 </div>
             </div>
         )
