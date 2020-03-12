b9f344ce4c9d911ca918ea8a3b43c55259cffb51
Ricardo Ribeiro Lopes <ricardo.sao@hotmail.com>
2020-03-11 21:31:06 -0300

    Resolver bug SplashScreen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Ecommerce from './pages/home/home';
+import Loading from 'react-loading';
 
 class SplashScreen extends Component {
   constructor() {
@@ -10,10 +11,11 @@ class SplashScreen extends Component {
       isSplash: false
     }
   }
+  //Setando intervalo para SplashScreen
   componentDidMount() {
     this.intervalID = setInterval(
       () => this.setState({ isSplash: true }),
-      2000
+      1500
     );
   }
   componentWillUnmount() {
@@ -27,20 +29,23 @@ class SplashScreen extends Component {
       right: 0,
       left: 0,
       width: '100vw',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column',
       background: 'rgba(235, 32, 73, 0.747)',
       position: 'fixed'
     };
 
+    //Verificando o state e renderizando condicionalmente
     if (this.state.isSplash == false) {
-      return <div style={style}></div>
+      return <div style={style}>
+        <h2 style={{ color: 'white', fontSize: '2.5em' }}>Carregando</h2>
+        <Loading color="white" type="spin" />
+      </div>
+    } else return this.props.history.push("/home")
 
-    } else {
-      return (
-        <div>
-          <Ecommerce />
-        </div>
-      );
-    }
+
   }
 }
 
--- src/Components/loading.js
+++ src/Components/loading.js
@@ -5,8 +5,7 @@ import ReactLoading from 'react-loading';
 export default class Loading extends Component {
     render() {
         return (
-            <div>                
-                <h1 style={{color: 'white', marginLeft: '22.5%' }}>{this.props.text}</h1>
+            <div >
                 <ReactLoading className="loading" type={this.props.type} color={this.props.color} height={this.props.height} width={this.props.width} />
             </div>
         )
